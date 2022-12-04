9e406c266a61470dd1c401b663ff7e677c43d1c2
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2022-12-04 22:57:42 +0900

    :tada: first commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    port: 8080,
+    open: true,
+  },
+  plugins: [
+    react({
+      jsxImportSource: '@emotion/react',
+    }),
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/test/setupTest.ts'],
+  },
+});
